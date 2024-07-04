86767b16fef632d082c27388b77e4b60d3d1743f
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-07-04 15:04:29 +0200

    Add card to about component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [NzIconModule],
+  imports: [NzIconModule, NzCardModule, NgOptimizedImage],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
-export class AboutPageComponent {}
+export class AboutPageComponent {
+  logoUrl = '/assets/makaDev.png';
+  logoAlt = 'logo';
+  imageUrl = 'cover.png';
+  imageAlt = 'Image makaDev';
+  githubUrl = '/assets/github.svg';
+  githubAlt = 'Image makaDev';
+  linkedInUrl = '/assets/linkedin.svg';
+  linkedInAlt = 'Image makaDev';
+}
