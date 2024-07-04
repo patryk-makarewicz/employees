402acafe8a1c7965343fc49828ba08054489c4a0
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-07-04 14:09:49 +0200

    Add info about progress

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-employees-page',
   standalone: true,
-  imports: [],
+  imports: [NzIconModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss',
 })
