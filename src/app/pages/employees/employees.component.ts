84bb4709a1e275c9f64c4d755b9fd77a55e4a44f
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-07-04 22:04:04 +0200

    Render data in basic table

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { EmployeesModel } from '../../../service/employees.model';
+import { EmployeesService } from '../../../service/employees.service';
 
 @Component({
   selector: 'app-employees-page',
   standalone: true,
-  imports: [NzIconModule],
+  imports: [NzIconModule, NzTableModule, NzDividerModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss',
 })
-export class EmployeesPageComponent {}
+export class EmployeesPageComponent implements OnInit {
+  employees: EmployeesModel[] = [];
+  errorMessage!: string;
+  isLoading = true;
+  isError = false;
+
+  constructor(private employees_service: EmployeesService) {}
+
+  ngOnInit() {
+    this.employees_service.getEmployeesList().subscribe({
+      next: (employees) => {
+        this.employees = employees.records;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.errorMessage = error;
+        this.isError = true;
+        this.isLoading = false;
+      },
+    });
+  }
+}
