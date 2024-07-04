85888c1f0e984e9418e4b90146308f52f266379a
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-07-04 09:42:47 +0200

    Add icon to home description

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { ButtonComponent } from '../../components';
+import { Router } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [NgOptimizedImage, ButtonComponent],
+  imports: [NgOptimizedImage, ButtonComponent, NzIconModule],
 })
 export class HomePageComponent {
   logoUrl = '/assets/logo.svg';
   logoAlt = 'Angular logo';
 
   title = 'Employees App';
-  description = 'Hi, this is app to manage employees 🎉';
+  description = 'Hi, this is the app to manage employees';
+
+  constructor(private router: Router) {}
 
   getStarted() {
-    console.log('logged');
+    this.router.navigate(['/employees']);
   }
 }
