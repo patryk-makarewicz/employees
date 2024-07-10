d2b3d1c4f0ea2ed8b7ec8b67aa39888b50735592
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-07-10 14:18:00 +0200

    Rename modal to add

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,5 +2,5 @@ export { HeaderComponent } from './header/header.component';
 export { FooterComponent } from './footer/footer.component';
 export { ButtonComponent } from './button/button.component';
 export { TableComponent } from './table/table.component';
-export { ModalComponent } from './modal/modal.component';
+export { AddModalComponent } from './modals/add/add.component';
 export { RemoveModalComponent } from './modals/remove/remove.component';
--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -4,14 +4,14 @@ import { catchError, finalize, switchMap, tap } from 'rxjs/operators';
 import { EmployeesModel } from '../../service/employees.model';
 import { EmployeesService } from '../../service/employees.service';
 import { NzMessageModule, NzMessageService } from 'ng-zorro-antd/message';
-import { ButtonComponent, TableComponent, ModalComponent } from '../../components';
+import { ButtonComponent, TableComponent, AddModalComponent } from '../../components';
 import { FormControl, FormGroup, Validators, NonNullableFormBuilder } from '@angular/forms';
 import { toastMessage, handleError, resetFormAndCloseModal, validateForm, getEmployeeFormFields } from '../../utils';
 
 @Component({
   selector: 'app-employees-page',
   standalone: true,
-  imports: [NzMessageModule, ButtonComponent, TableComponent, ModalComponent],
+  imports: [NzMessageModule, ButtonComponent, TableComponent, AddModalComponent],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss',
 })
@@ -193,7 +193,7 @@ export class EmployeesPageComponent implements OnInit {
       .subscribe();
   }
 
-  openAddEmployeeModal(): void {
+  onOpenAddEmployeeModal(): void {
     this.isModalAddEmployeeVisible = true;
     this.editingEmployeeId = null;
     this.createEmployeeForm.reset();
